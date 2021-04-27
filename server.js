b5478e4e9c7b66e5b82d23d2cc47bde08aba7fe7
N-Pacis <pacisnkubito@gmail.com>
2021-04-27 21:09:58 +0200

    Initial commit and project setup

--- server.js
+++ server.js
@@ -7,9 +7,21 @@ const dbErrors = debug('db:errors')
 const error = debug('error')
 const bodyParser = require('body-parser')
 const config = require('config')
+const mongoose = require('mongoose')
 
 //calling the bodyParser middleware that help us receive form values from the forms
 app.use(bodyParser.urlencoded({extended:true}))
+
+//connecting to the database
+let password = config.get("DATABASE_PASSWORD")
+mongoose.connect(`mongodb+srv://talentar-backend-team:talentar12345@talentar-backend.m9eua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,{useCreateIndex:true,useNewUrlParser:true,useFindAndModify:false,useUnifiedTopology:true})
+    .then(()=>{
+        dbDebug("Connected to the database successfully...")
+    })
+    .catch(err =>{
+        dbErrors("Failed to connect due to ",err)
+        console.log(config.get("DATABASE_PASSWORD"))
+    })
 
 //listening to the port and checking if the PORT environment variable is set
 if(!config.get("PORT")){
